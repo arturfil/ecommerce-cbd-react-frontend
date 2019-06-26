b9bea3210f3bb058153ff0e0793555002a39599e
Arturo Filio Villa <arturfil@hotmail.com>
2019-06-26 03:20:43 -0400

    created the dashboard and the protected routes depending on user role

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './user/Signup'
 import Signin from './user/Signin'
 import Home from './core/Home'
+import PrivateRoute from './auth/PrivateRoute';
+import AdminRoute from './auth/AdminRoute';
+import Dashboard from './user/UserDashboard';
+import AdminDashboard from './user/AdminDashboard';
+
 
 const Rotues = () => {
   return (
@@ -11,6 +16,8 @@ const Rotues = () => {
           <Route path="/" exact component={Home} />
           <Route path="/signin" exact component={Signin}/>
           <Route path="/signup" exact component={Signup} />
+          <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+          <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
         </Switch>
     </BrowserRouter>
   )
--- src/core/Menu.js
+++ src/core/Menu.js
@@ -14,15 +14,40 @@ const Menu = ({history}) => (
   <div>
     <ul className="nav nav-tabs bg-primary">
       <li className="nav-item">
-        <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
+        <Link 
+          className="nav-link" 
+          style={isActive(history,"/")}
+          to="/">Home
+        </Link>
       </li>
+      
+      {isAuthenticated() && isAuthenticated().user.role === 1 && (
+        <li className="nav-item">
+          <Link className="nav-link"
+            style={isActive(history, '/admin/dashboard')}
+            to="/admin/dashboard">Dashboard</Link>
+        </li>
+      )}
+
+      {isAuthenticated() && isAuthenticated().user.role === 0 && (
+        <li className="nav-item">
+          <Link className="nav-link"
+            style={isActive(history, '/user/dashboard')}
+            to="/user/dashboard">Dashboard</Link>
+        </li>
+      )}
+
       {!isAuthenticated() && (
         <Fragment>
           <li className="nav-item">
-            <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
+            <Link className="nav-link" 
+              style={isActive(history, '/signup')} 
+              to="/signup">Signup</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">SignIn</Link>
+            <Link className="nav-link" 
+              style={isActive(history, '/signin')} 
+              to="/signin">SignIn</Link>
           </li>
         </Fragment>
       )}
