6db8e07d377d225530ab00a920669609daa3a6dd
Arturo Filio Villa <arturfil@hotmail.com>
2019-07-22 11:27:29 -0400

    refactoring the admin/order route

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -3,6 +3,7 @@ import Layout from '../core/Layout';
 import {isAuthenticated } from '../auth';
 import { Link } from "react-router-dom";
 import { listOrders } from './apiAdmin';
+import moment from 'moment';
 
 const Orders = () => {
   const [orders, setOrders] = useState([])
@@ -20,9 +21,22 @@ const Orders = () => {
   useEffect(() => {
     loadOrders()
   }, []);
-  const noOrders = orders => {
-    return orders.length < 1 ? <h4>No orders</h4> : null;
+  
+  const showOrdersLenght = () => {
+    if(orders.length > 0) {
+      return (
+        <h1 className="text-danger display-2">Total orders: {orders.length}</h1>
+      )
+    } else {
+      return <h1 className="text-danger">No orders</h1>
+    }
   };
+
+  const showInput = (key,value) => (
+    <div className="input-group mb-2 mr-sm-2">
+      <div></div>
+    </div>
+  )
 
   return (
     <Layout
@@ -30,8 +44,48 @@ const Orders = () => {
       description={`${user.name}'s Orders Dashboard`}>
       <div className="row">
         <div className="col-md-8 offset-md-2">
-          {noOrders(orders)}
-          {JSON.stringify(orders)}
+          {showOrdersLenght(orders)}
+          {orders.map((o,oIndex) => {
+            return (
+              <div className="mt-5" key={oIndex} style={{borderBottom: "5px solid indigo"}}>
+                <h2 className="mb-5">
+                  <span className="bg-primary">Order ID: {o._id}</span>
+                </h2>
+                <ul className="list-group mb-2">
+                  <li className="list-group-item">
+                    {o.status}
+                  </li>
+                  <li className="list-group-item">
+                    Transaction ID: {o.transaction_id}
+                  </li>
+                  <li className="list-group-item">
+                    Amount Total: ${o.amount}
+                  </li>
+                  <li className="list-group-item">
+                    Ordered By: {o.user.name}
+                  </li>
+                  <li className="list-group-item">
+                    Ordered on: {moment(o.createdAt).fromNow()}
+                  </li>
+                  <li className="list-group-item">
+                    Delivery Address {o.address}
+                  </li>
+                </ul>
+                <h3 className="mt-4 mb-4 font-italic">
+                  Total products in the order: {o.products.length}
+                </h3>
+
+                {o.products.map((p,pIndex) => (
+                  <div className="mb-4" key={pIndex} style={{padding: '20px', border: '1px solid indigo'}}>
+                    {showInput('Product name', p.name)}
+                    {showInput('Product price', p.price)}
+                    {showInput('Product total', p.total)}
+                    {showInput('Product id', p._id)}
+                  </div>
+                ))}
+              </div>
+            )
+          })}
         </div>
       </div>
     </Layout>
