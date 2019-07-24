a67636826daa420e5098780cc43ca8a5d951b7fb
Arturo Filio Villa <arturfil@hotmail.com>
2019-07-24 02:22:17 -0400

    added a few css classes to make the orders more readable

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth'
 import { Link } from 'react-router-dom';
+import {getPurchaseHistory} from './apiUser';
+import moment from 'moment';
 
 const Dashboard = () => {
+  const [history,setHistory] = useState([])
   const {user : {_id, name, email, role}} = isAuthenticated();
+  const token = isAuthenticated().token;
+
+  const init = (userId, token) => {
+    getPurchaseHistory(userId, token).then(data => {
+      if(data.error) {
+        console.log(data.error);
+      } else {
+        setHistory(data);
+      }
+    })
+  }
+
+  useEffect(() => {  
+    init(_id,token)
+  })
 
   const userLinks = () => {
     return (
@@ -36,12 +54,35 @@ const Dashboard = () => {
     )
   }
 
-  const purchaseHistory = () => {
+  const purchaseHistory = (history) => {
     return (
       <div className="card mb-5">
         <h3 className="card-header">Purchase history</h3>
         <ul className="list-group">
-          <li className="list-group-item">history</li>
+          <li className="list-group-item">
+            {history.map((h,i) => {
+              return (
+                <div className="card mb-3">
+                  
+                  <h6 className="card-header" style={{ backgroundColor: "lightgrey" }}>Order# {h._id}</h6>
+                    {h.products.map((p, i) => {
+                      return (
+                        <div className="m-3">
+                          {
+                            <div key={i} style={{ color: "grey" }}>
+                              <h6>Product Name: {p.name}</h6>
+                              <h6>Product Price: ${p.price}</h6>
+                              <h6>Purchased date: {moment(p.createdAt).fromNow()}</h6>
+                            </div>
+                          }
+                        </div>
+                      )
+                    })}
+                  
+                </div>
+              )
+            })}
+          </li>
         </ul>
       </div>
     )
@@ -58,7 +99,7 @@ const Dashboard = () => {
           </div>
           <div className="col-9">
             {userInfo()}
-            {purchaseHistory()}
+            {purchaseHistory(history)}
           </div>
         </div>
     </Layout>
