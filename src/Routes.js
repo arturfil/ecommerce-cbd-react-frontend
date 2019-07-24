98895acea2842d9d8daafa2c9d06c3507170a639
Arturo Filio Villa <arturfil@hotmail.com>
2019-07-24 01:35:58 -0400

    added a new file Profile and new routes along with methods in order for the user to be able to updated its profile

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,7 @@ import Shop from './core/Shop';
 import PrivateRoute from './auth/PrivateRoute';
 import AdminRoute from './auth/AdminRoute';
 import Dashboard from './user/UserDashboard';
+import Profile from './user/Profile';
 import AdminDashboard from './user/AdminDashboard';
 import AddCategory from './admin/AddCategory';
 import AddProduct from './admin/AddProduct';
@@ -25,6 +26,7 @@ const Rotues = () => {
           <Route path="/product/:productId" exact component={Product} />
           <Route path="/cart" exact component={Cart} />
           <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+          <PrivateRoute path="/profile/:userId" exact component={Profile} />
           <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
           <AdminRoute path="/admin/orders" exact component={Orders} />
           <AdminRoute path="/create/category" exact component={AddCategory} />
