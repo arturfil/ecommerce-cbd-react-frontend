5ba78637c62f4f86ce673cf70c8659bd8301be28
Arturo Filio Villa <arturfil@hotmail.com>
2019-07-30 02:40:48 -0400

    formatted the auth provider

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,7 +1,7 @@
 import {API} from '../config';
 
 export const signup = user => {
-  return fetch(`${API}/api/users/signup`, {
+  return fetch(`${API}/users/signup`, {
     method: "POST",
     headers: {
       Accept: 'application/json',
@@ -18,7 +18,7 @@ export const signup = user => {
 };
 
 export const signin = user => {
-  return fetch(`${API}/api/users/signin`, {
+  return fetch(`${API}/users/signin`, {
     method: "POST",
     headers: {
       Accept: 'application/json',
@@ -45,7 +45,7 @@ export const signout = (next) => {
   if (typeof window !== 'undefined') {
     localStorage.removeItem('jwt');
     next();
-    return fetch(`${API}/api/users/signout`, {
+    return fetch(`${API}/users/signout`, {
       method: 'GET',
     })
       .then(response => {
